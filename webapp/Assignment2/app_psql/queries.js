6484063f313969bc1b28c224d9a6c486187f2000
ajay <ajaygoel1993@gmail.com>
2019-09-22 20:10:25 -0400

    Password NIST and email validation
    Password NIST and Email validation, structure of update

--- webapp/Assignment2/app_psql/queries.js
+++ webapp/Assignment2/app_psql/queries.js
@@ -1,25 +1,23 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> bd999bf2ae2d3eff99b50a7c3f16804097267f83
   user: 'me',
   host: 'localhost',
   database: 'api',
   password: 'Ajaygoel@123',
-<<<<<<< HEAD
-=======
-  user: "me",
-  host: "localhost",
-  database: "api",
-  password: "Ajaygoel@123",
->>>>>>> 8e08598173dfaf3eb0a01efcec7a9a1999760061
-=======
->>>>>>> bd999bf2ae2d3eff99b50a7c3f16804097267f83
   port: 5432,
 });
 const bcrypt = require("bcrypt");
+var validator = require("email-validator");
+var passwordValidator = require('password-validator');
+var schema = new passwordValidator();
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits()                                 // Must have digits
+.has().not().spaces()                           // Should not have spaces
+.is().not().oneOf(['Passw0rd', 'Password123']);
 
 const getUsers = (request, response) => {
     console.log("Here");
@@ -45,7 +43,7 @@ const getUsers = (request, response) => {
      const { first_name, last_name, email, password } = request.body;
      const created_date = Date.now;
      const updated_date = Date.now;
-
+     
      console.log("Here");
         // pool.query('INSERT INTO users (first_name, last_name,email,password ) VALUES ($1,$2,$3,$4)'
         // , [first_name, last_name, email, password ], (error,results) => {
@@ -61,42 +59,82 @@ const getUsers = (request, response) => {
     // const { first_name, last_name,email,password } = request.body;
     // const created_date = Date.now;
     // const updated_date = Date.now;
-    bcrypt.hash(request.body.password, 10, (err, hash) => { 
-        if (err) {
-          return res.status(500).json({
-            error: err
-          });
-        } else {
-            pool.query('INSERT INTO users (first_name, last_name,email,password ) VALUES ($1,$2,$3,$4)'
-            , [first_name, last_name, email, hash ], (error,results) => {
-            if (error) {
-                console.log(error);
-                throw error
+    const email_check = request.body.email;
+    const password_check = request.body.password;
+    console.log(schema.validate(request.body.password));
+    console.log(email_check);
+
+    if(validator.validate(email_check) && schema.validate(password_check)){
+    
+    pool.query('SELECT * FROM users WHERE email = $1', [email_check], (error, results) => {
+      if (results.rows.length>=1) {
+        response.status(400).json({
+            message: "User Email exists..!!"
+        });
+        //throw error;
+      } else {
+        bcrypt.hash(request.body.password, 10, (err, hash) => { 
+            if (err) {
+              return res.status(500).json({
+                error: err
+              });
+            } else {
+                pool.query('INSERT INTO users (first_name, last_name,email,password ) VALUES ($1,$2,$3,$4)'
+                , [first_name, last_name, email, hash ], (error,results) => {
+                if (error) {
+                    console.log(error);
+                    throw error
+                }
+                response.status(201).json({
+                    message: "User added",
+                    details: request.body,
+                    password: hash
+                });
+                });
             }
-            response.status(201).json({
-                message: "User added",
-                details: request.body,
-                password: hash
-            });
-            });
-        }
+        });
+      }
+
+      //response.status(200).json(results.rows)
+    })
+} else {
+    response.status(401).json({
+        message: "Invalid Email or password..!!",
+        "password_guidelines: ": ["Minimum length 8","Maximum length 100","Must have uppercase letters",
+                            "Must have lowercase letters","Must have digits","Should not have spaces"],
+        "Status code": 401
     });
-  }
+}
+}
 
   const updateUser = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { name, email } = request.body
-  
+    const email = request.body.email;
+    console.log(email);
+    const { first_name, last_name, password } = request.body;
     pool.query(
-      'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-      [name, email, id],
+      'UPDATE users SET first_name = $1, last_name = $2, password =$3 WHERE email = $4',
+      [first_name, last_name, password, email],
       (error, results) => {
         if (error) {
           throw error
         }
-        response.status(200).send(`User modified with ID: ${id}`)
+
+        response.status(200).send(`User modified with ID: ${email}`)
       }
     )
+    // const id = parseInt(request.params.id)
+    // const { name, email } = request.body
+  
+    // pool.query(
+    //   'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    //   [name, email, id],
+    //   (error, results) => {
+    //     if (error) {
+    //       throw error
+    //     }
+    //     response.status(200).send(`User modified with ID: ${id}`)
+    //   }
+    // )
   }
 
   const deleteUser = (request, response) => {
@@ -109,7 +147,6 @@ const getUsers = (request, response) => {
       response.status(200).send(`User deleted with ID: ${id}`)
     })
   }
-
 
   module.exports = {
     getUsers,
