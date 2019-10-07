a736baea16c0f962271c2d6222cdfbb43ab28ff3
Ajay Goel <ajaygoel1993@gmail.com>
2019-10-07 10:15:50 -0400

    Added 2 more subnets

--- infrastructure/aws/terraform/main.tf
+++ infrastructure/aws/terraform/main.tf
@@ -17,6 +17,16 @@ variable "subnet_cidr_block" {
   default = "10.0.1.0/24"
 }
 
+variable "subnet_cidr_block2" {
+  type = string
+  default = "10.0.2.0/24"
+}
+
+variable "subnet_cidr_block3" {
+  type = string
+  default = "10.0.3.0/24"
+}
+
 resource "aws_vpc" "vpc" {
     cidr_block                          =   var.cidr_block
     enable_dns_hostnames                =   true
@@ -29,12 +39,32 @@ resource "aws_vpc" "vpc" {
 }
 
 resource "aws_subnet" "subnet" {
-    cidr_block                          =   var.cidr_block
+    cidr_block                          =   var.subnet_cidr_block
     vpc_id                              = aws_vpc.vpc.id
     availability_zone                   = "us-east-1a"
     map_public_ip_on_launch             = true
     tags        =   {
         Name                            = "csye6225-subnet"   
+    }   
+}
+
+resource "aws_subnet" "subnet2" {
+    cidr_block                          =   var.subnet_cidr_block2
+    vpc_id                              = aws_vpc.vpc.id
+    availability_zone                   = "us-east-1b"
+    map_public_ip_on_launch             = true
+    tags        =   {
+        Name                            = "csye6225-subnet2"   
+    }   
+}
+
+resource "aws_subnet" "subnet3" {
+    cidr_block                          =   var.subnet_cidr_block3
+    vpc_id                              = aws_vpc.vpc.id
+    availability_zone                   = "us-east-1c"
+    map_public_ip_on_launch             = true
+    tags        =   {
+        Name                            = "csye6225-subnet3"   
     }   
 }
 
