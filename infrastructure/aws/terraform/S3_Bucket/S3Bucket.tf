3f3cc68e5f7387d38797edbc0e4272e9cd211c95
Ajay Goel <ajaygoel1993@gmail.com>
2019-10-23 01:07:20 -0400

    S3 bucket

--- infrastructure/aws/terraform/S3_Bucket/S3Bucket.tf
+++ infrastructure/aws/terraform/S3_Bucket/S3Bucket.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 resource "aws_s3_bucket" "bucket" {
-  bucket = "webapp.dev.akshaymahajanshetti.me"
+  bucket = "webapp.dev.ajaygoel.me"
   force_destroy = true
   acl = "private"
   server_side_encryption_configuration {
@@ -12,6 +13,13 @@ resource "aws_s3_bucket" "bucket" {
       }
     }
  }
+    cors_rule {
+    allowed_headers = ["Authorization"]
+    allowed_methods = ["GET", "POST", "DELETE"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 
   lifecycle_rule {
     enabled = true
@@ -21,4 +29,32 @@ resource "aws_s3_bucket" "bucket" {
       storage_class = "STANDARD_IA"
     }
   }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = "${aws_s3_bucket.bucket.id}"
+  name = "${aws_s3_bucket.name}"
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Id": "Policy1488494182833",
+    "Statement": [
+        {
+            "Sid": "Stmt1488493308547",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::737362253087:user/Administrator"
+            },
+            "Action": [
+                "s3:ListBucket",
+                "s3:ListBucketVersions",
+                "s3:GetBucketLocation",
+                "s3:Get*",
+                "s3:Put*"
+            ],
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.name}"
+        }
+    ]
+}
+POLICY
 }
