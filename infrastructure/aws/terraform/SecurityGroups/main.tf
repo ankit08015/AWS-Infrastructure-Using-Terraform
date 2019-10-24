8d82265f1aa2846700f422225d0467f21388128c
Ajay Goel <ajaygoel1993@gmail.com>
2019-10-23 23:43:24 -0400

    added egress

--- infrastructure/aws/terraform/SecurityGroups/main.tf
+++ infrastructure/aws/terraform/SecurityGroups/main.tf
@@ -97,6 +97,12 @@ resource "aws_security_group" "allow_tls" {
     description = "PORT 3000"
     cidr_blocks = [var.cidr_block_3000]
   }
+    egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 // DATABASE SECURITY GROUP
