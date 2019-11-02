b6f8f1f5a6c221479b5db12d07805c8683c7fba1
Ajay Goel <ajaygoel1993@gmail.com>
2019-11-01 21:50:50 -0400

    deploy

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -2,6 +2,3 @@
 ##### CLOUD WATCH AGENT
 
 # run the node js application
-cd /home/centos/ccwebapp/webapp/app_psql
-
-npm install
--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # doing npm install for the webapp
 sudo mv home/centos/.env /home/centos/ccwebapp/webapp/app_psql/
+
+cd /home/centos/ccwebapp/webapp/app_psql
+
+npm install
