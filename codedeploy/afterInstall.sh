46a8da63619f66e356665880d99f548adb55edb5
Ajay Goel <ajaygoel1993@gmail.com>
2019-11-02 01:23:02 -0400

    CICD

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd /home/centos/
+./confAws.sh
+
 # Further steps: like to stop the application later on but no step as of now.
 #/home/centos/./confAws.sh
 #cd /home/centos/ccwebapp/webapp/app_psql
