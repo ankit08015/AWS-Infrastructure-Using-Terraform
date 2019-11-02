232f84aec4dbeeab37ee78f4606da45856fcf722
Ajay Goel <ajaygoel1993@gmail.com>
2019-11-01 23:25:30 -0400

    CICD test

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # doing npm install for the webapp
-sudo mv home/centos/.env /home/centos/ccwebapp/webapp/app_psql/
+sudo mv /home/centos/.env /home/centos/ccwebapp/webapp/app_psql/
 
 chmod 777 /home/centos/confAws.sh 
-
-(cd /home/centos && ./confAws.sh)
+/home/centos/./confAws.sh
