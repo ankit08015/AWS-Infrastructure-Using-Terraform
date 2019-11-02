79d784c440711401143fafef878c06fd9d2298da
Ajay Goel <ajaygoel1993@gmail.com>
2019-11-01 23:04:15 -0400

    aws configure CICD

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -2,6 +2,12 @@
 # doing npm install for the webapp
 sudo mv home/centos/.env /home/centos/ccwebapp/webapp/app_psql/
 
+sudo chmod +x /home/centos/confAws.sh 
+
+cd /home/centos
+
+./confAws.sh
+
 cd /home/centos/ccwebapp/webapp/app_psql
 
 npm install
