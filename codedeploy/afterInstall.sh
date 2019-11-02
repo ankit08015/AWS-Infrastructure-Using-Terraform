d5f76ca1896b995b362aa90da51d33d14244ec2b
Ajay Goel <ajaygoel1993@gmail.com>
2019-11-02 12:15:46 -0400

    added config

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-cd /home/centos/
-./confAws.sh
+#cd /home/centos/
+#./confAws.sh
 
 # Further steps: like to stop the application later on but no step as of now.
 #/home/centos/./confAws.sh
