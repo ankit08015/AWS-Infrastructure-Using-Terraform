746053b73263a2124f4ddf0a4b99d455ab50fb31
Ajay Goel <ajaygoel1993@gmail.com>
2019-11-03 18:46:59 -0500

    COCD v12

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -5,8 +5,6 @@
 #/home/centos/./confAws.sh
 
 mv /home/centos/.env /home/centos/ccwebapp/webapp/app_psql
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
-
 cd /home/centos/ccwebapp/webapp/app_psql
 npm install
 sudo npm install forever -g
--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -2,6 +2,7 @@
 ##### CLOUD WATCH AGENT
 
 # run the node js application
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
 cd /home/centos/ccwebapp/webapp/app_psql
 forever start index.js
 # node index.js
