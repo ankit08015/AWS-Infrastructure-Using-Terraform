1d5457aaa32ac293d4651a91ab2f3856034e4f2a
Ajay Goel <ajaygoel1993@gmail.com>
2019-11-04 19:08:29 -0500

    CICD Linked

--- codedeploy/beforeInstall.sh
+++ codedeploy/beforeInstall.sh
@@ -2,9 +2,4 @@
 #sudo chmod 777 /home/centos/confAws.sh
 #cd /home/centos/
 #./confAws.sh
-mv /home/centos/.env /home/centos/ccwebapp/webapp/app_psql
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
-
-cd /home/centos/ccwebapp/webapp/app_psql
-sudo forever stop index.js
-forever start index.js
+cd /home/ccwebapp
