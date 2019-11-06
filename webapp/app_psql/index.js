9df67a641d71bd86d8cf24b9721c179842a7447a
Akshay Mahajanshetti <mahajanshetti.a@husky.neu.edu>
2019-11-05 19:34:45 -0500

    CICD

--- webapp/app_psql/index.js
+++ webapp/app_psql/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 morgan = require('morgan')
 const app = express()
-
 const PORT = 3000;
 
 const busboy = require('connect-busboy');
